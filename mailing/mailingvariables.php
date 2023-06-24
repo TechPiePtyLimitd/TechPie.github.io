b103ad73cab9aaba4700fc942f3fa84da9036a4e
SandeepKumar <Sandeepk@strate.co.za>
2023-06-24 21:34:08 +0200

    ne web

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "smtp.gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = "sande.vit@gmail.com"; //sender
+    $mail_sender_password = "Srtend1"; //sender
+    $mail_sender_name = "Website Form";
+?>
